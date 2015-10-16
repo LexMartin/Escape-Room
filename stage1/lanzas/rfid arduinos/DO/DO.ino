80a33272c59f89974ba6de1d28b9f608645b0913
Alejandro Martin <lexmartin@xandar.local>
2015-10-16 13:11:01 -0500

    Funciona correcta de PRESENT_PIN y CORRECT_PIN
    Se reestructura código y se crea función para verificar antes de leer y
    comprobar la presencia o ausencia de tags. Se maximiza el alcance de la
    antena

--- stage1/lanzas/rfid arduinos/DO/DO.ino
+++ stage1/lanzas/rfid arduinos/DO/DO.ino
@@ -10,28 +10,30 @@
 // byte value[] = {0x40, 0xef, 0xf5, 0x6a};
 byte value[] = {0x19, 0x52, 0x85, 0x9e};
 
+
+// Puertos de salida hacia el Arduino Principal
+// Modificar solo si es necesario
+#define PRESENT_PIN     7
+#define CORRECT_PIN     6
+
 //=================================================================
 
 #define RST_PIN         9          
-#define SS_PIN          10          
-#define PRESENT_PIN     8
-#define CORRECT_PIN     7
+#define SS_PIN          10
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);   //Se crea el objeto MFRC522
 MFRC522::MIFARE_Key key;
 
-byte last[] = {0x00,0x00,0x00,0x00};
-byte status;
+byte empty[] = {0x00,0x00,0x00,0x00};
 byte buffer[4];
-byte size = sizeof(buffer);
-
-
+byte buffersize = sizeof(buffer);
 
 void setup() {
     Serial.begin(9600); 
     while (!Serial);    // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
     SPI.begin();        // Init SPI bus
     mfrc522.PCD_Init(); // Init MFRC522 card
+    mfrc522.PCD_SetRegisterBitMask(mfrc522.RFCfgReg, (0x07<<4)); //Set Antena Gain to MAX
 
     // Prepare the key (used both as key A and as key B)
     // using FFFFFFFFFFFFh which is the default at chip delivery from the factory
@@ -46,53 +48,38 @@ void setup() {
 }
 
 void loop() {
+  
+  ifCorrect();  
+   
     // Look for new cards
     if ( ! mfrc522.PICC_IsNewCardPresent()){
         digitalWrite(PRESENT_PIN,LOW);
+        *buffer = *empty;
         return;
     }
     digitalWrite(PRESENT_PIN,HIGH);
-    // Select one of the cards
     if ( ! mfrc522.PICC_ReadCardSerial())
         return;
 
     //Realiza la lectura del uID del Tag
     for (int i = 0; i < 4; i++) {  //
     buffer[i] = mfrc522.uid.uidByte[i];
-    //Serial.print(readCard[i], HEX);   //Debug
   }
-    //Serial.println();                   //Debug
-   
-     
-    if(compareByte(buffer,sizeof(value),value))
-    {
-      digitalWrite(CORRECT_PIN,HIGH);
-      Serial.println("TRUE");
-    }
-    else
-    {
-      digitalWrite(CORRECT_PIN,LOW);
-      Serial.println("FALSE");
-    }
 }
 
 
-bool compareByte(byte *buffer, byte bufferSize , byte *value) {
-    int cont = 0;
-    for (byte i = 0; i < bufferSize; i++) {
-        last[i] = value[i];
-        if(buffer[i]!=value[i])
-        {
-          cont = 1;
-        }
-    }
+bool compareByte(byte *buffer,  byte *value) {
+    for (byte i = 0; i < buffersize; i++) 
+        if(buffer[i]!=value[i]) 
+            return false;
+     return true;
+}
 
-    if(cont == 0)
-    {
-      return true;
-    }
-    else
-    {
-      return false;
+void ifCorrect(){
+  if(compareByte(buffer,value)){
+    digitalWrite(CORRECT_PIN,HIGH);
+    } 
+  else {
+    digitalWrite(CORRECT_PIN,LOW);
     }
 }
