c1de2de41e9a88dc51988dd76889857b13b73e47
Cristian Kumul <cristian.kumul@gmail.com>
2015-12-08 01:50:19 -0500

    Lamparas con sensores de peso

--- stage1/lamparas/Pesos/Pesos.ino
+++ stage1/lamparas/Pesos/Pesos.ino
@@ -15,31 +15,90 @@
 
 
 
-HX711 scale(A1, A0);	// parameter "gain" is ommited; the default value 128 is used by the library
-HX711 Peso1(A2, A3);
-
-const float Umbral=30.0;
+HX711 Peso1(A1, A0);  
+HX711 Peso2(A3, A2);
+HX711 Peso3(A5, A4);
+HX711 Peso4(A7, A6);
+HX711 Peso5(A9, A8);
+HX711 Peso6(A11, A10);
+HX711 Peso7(A13, A11);
+HX711 sensors[] = {Peso1,Peso2,Peso3,Peso4,Peso5,Peso6,Peso7};
+const float Umbral=300.0;
 const int PIN_OK = 4;
 
 float Temp=0;
-
+float umbrals[] = {0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+int correctUmbral[] = {0,0,0,0,0,0,0};
 void setup() {
   Serial.begin(38400);
 pinMode(PIN_OK,OUTPUT);
-scale.set_scale(2280.f);                      // this value is obtained by calibrating the scale with known weights; see the README for details
-  scale.tare();		
+//scale.set_scale(2280.f);                      // this value is obtained by calibrating the scale with known weights; see the README for details
+//  scale.tare();		
+Serial.print("SETUP");
+float cal2 = 0;
+ for (int i = 0; i < 7 ; i++){
+      sensors[i].set_scale(107.f);
+      sensors[i].tare();
+   }
+ Serial.print("Termina Setup");
+ getUmbrals();
 }
 
 void loop() {
-  Temp=scale.get_units(3);
-  Serial.println(Temp,1);
-  if (Temp>Umbral)
-    digitalWrite(PIN_OK,HIGH);
-    else 
-    digitalWrite(PIN_OK,LOW);
-    
+  //Serial.println("iNICIO DEL LOOP");
+
+  int i = 0;
+  int foo = readPIR2(i);
+  Serial.println("");
+  Serial.print("Lampara ");
+  Serial.print(i);
+  Serial.print(":");
+  Serial.print(foo);
+
+}
+
+
+float readPIR2(int index){
+  float tmp = pow(sensors[index].get_units(3),2);
+  float dif = tmp - umbrals[index];
+  if(dif > 35000.0 && correctUmbral[index] == 0){
+    umbrals[index] = tmp;
+    correctUmbral[index] = 1;
+  }
   
-  scale.power_down();			        // put the ADC in sleep mode
-  //delay(500);
-  scale.power_up();
+  
+  float compare = sqrt(pow(tmp -umbrals[index],2));
+  
+  float estimate = umbrals[index]+(umbrals[index]* 0.05); 
+  
+  int result = 0;
+  
+  result = compare > 20000.0 ? 1:0;
+  
+  sensors[index].power_down();              // put the ADC in sleep mode
+  //delay(5000);
+  sensors[index].power_up();
+  return tmp;  
+}
+int readPIR(int index)
+{
+  
+  float tmp = sensors[index].get_units(3);
+  
+  int result = tmp > Umbral ? 1:0;
+  sensors[index].power_down();              // put the ADC in sleep mode
+  //delay(5000);
+  sensors[index].power_up();
+  return result;  
+}
+
+
+void getUmbrals(){
+   float cal2 = 0;
+   for (int i = 0; i < 7; i++){
+  
+        cal2=sensors[i].get_units(3);
+         umbrals[i] = pow(cal2,2);
+          delay(100);     
+   }
 }
--- stage1/lamparas/lamparas.ino
+++ stage1/lamparas/lamparas.ino
@@ -1,24 +1,60 @@
 
 // Created by Cindy Canul Canul & Cristian Kumul Uc
 // E-mail: cindycanul92@gmail.com, cristiankumul@gmail.com
+#include "HX711.h"
 #include <SoftwareSerial.h>
 #include <MP3.h>
+
+
 /** define mp3 class */
-MP3 mp3;
-const int _n_levels=5;                //Número de elementos que contendrá la serie para ganar el juego
-// Variables
+
+//
+//  Sensor de Peso 50Kg --------- HX711
+//  Blanco ----------------------- E+
+//  Negro  ----------------------- E- 
+//  Blanco ---Resistencia---+
+//                          +----- A-
+//  Negro  ---Resistencia --+
+//  Rojo   ----------------------- A+
+
+//-------------CONFIGURACIONES---------------------------------------------------------------------------------------------------------------//
+const int _n_levels=10;                //Número de elementos que contendrá la serie para ganar el juego
 int inputPin[] = {2,3,4,5};           //Declaración de las entradas que se usarán
-int ledPin[] = {9,10,11,12};            //Pines para los leds de la secuencia
+int ledPin[] = {22,24,26,28};            //Pines para los leds de la secuencia
+long interval = 3 * 1000;                   //Intervalo de tiempo que se le permitirá al usuario entre cada entrada en ms
+const unsigned char s[] = {0x0001,0x0002,0x0003,0x0004,0x0005,0x0006,0x0007,0x0008,0x0009,0x0010};//Sonidos
+const int soundStorage = 0; //almacenamiento de los sonidos , 0:USB , 1:SD
+int secretCompartment = 35;
+//------------/CONFIGURACIONES---------------------------------------------------------------------------------------------------------------//
+
+
+
+
+
+//Sensores de peso
+HX711 Peso1(A1, A0);  
+HX711 Peso2(A3, A2);
+HX711 Peso3(A5, A4);
+HX711 Peso4(A9, A8);
+HX711 Peso5(A11, A10);
+HX711 Peso6(A13, A12);
+HX711 Peso7(A15, A14);
+HX711 sensors[] = {Peso1,Peso2,Peso3,Peso4,Peso5,Peso6,Peso7};
+const float Umbral=300.0;
+
+MP3 mp3;
+
 int buttonState[] = {0,0,0,0};         // El estado actual de los botones
 int lastButtonState[] = {0,0,0,0};     // El estado previo de los botones
 int serie_array[_n_levels],currentValue; //vectores donde se almacenará la serie y currentValue es donde almacenaremos el valor de la selección del usuario en tiempo de ejecución
 int readyLed = 6;                      //Led que nos dará señal que espera la entrada del usuario
 long previousMillis = 0;                //Variable para determinar el tiempo que ha pasado mientras el usuario no activa una entrada
-long interval = 7000;                   //Intervalo de tiempo que se le permitirá al usuario entre cada entrada en ms
-const unsigned char s[] = {0x0001,0x0002,0x0003,0x0004,0x0005,0x0006,0x0007,0x0008,0x0009,0x0010};//Sonidos
-const int soundStorage = 1; //almacenamiento de los sonidos , 0:USB , 1:SD
+float umbrals[] = {0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+int correctUmbral[] = {0,0,0,0,0,0,0};
 
-
+int game_on = 0;
+int currentlevel = 0; // es el nivel actual, es decir el numero de entradas correctas del usuario
+int n_levels =_n_levels; //niveles (se asigna el valor de _n_levels a n_levels por cuestiones de crear la memoria dinámicamente)
 
 
 
@@ -33,20 +69,40 @@ void setup() {
     pinMode(inputPin[x],INPUT);
     pinMode(ledPin[x],OUTPUT);
   }
-  pinMode(readyLed,OUTPUT);
+
+  for(int x = 0; x < 4; x++)
+  { 
+    digitalWrite(ledPin[x],HIGH);
+    //delay(400);
+    //digitalWrite(ledPin[x],LOW);
+    
+  }
+  delay(400);
+  for(int x = 0; x < 4; x++)
+  { 
+    //digitalWrite(ledPin[x],HIGH);
+    //delay(400);
+    digitalWrite(ledPin[x],LOW);
+    
+  }
+  pinMode(secretCompartment,OUTPUT);
+
+  //Iniciar la escala de los sensores
+  
+
+ 
   // iniciamos la comunicación serial para imprimir por la salida estandar
   Serial.begin(9600); 
+
+     float cal2 = 0;
+   for (int i = 0; i < 7; i++){
+      sensors[i].set_scale(2280.f);
+      sensors[i].tare();
+    
+   }
+   getUmbrals();
+   
 }
-int game_on = 0;
-int currentlevel = 1; // es el nivel actual, es decir el numero de entradas correctas del usuario
-long rand_num = 0; //variable donde se almacena un numero pseudorandom
-int rando = 0; 
-int ledtime = 500; //tiempo que el led estará encendido cuando el usuario seleccione un botón
-int pinLedSerie = 0; //Variable para almacenar el led que se encenderá al mostrar la secuencia
-int right = 0; //1 indica que el usuario ingresó un dato correcto , 0 indica que ha fallado
-int speedfactor = 5; 
-int leddelay = 200; //delay de los leds
-int n_levels =_n_levels; //niveles (se asigna el valor de _n_levels a n_levels por cuestiones de crear la memoria dinámicamente)
 
 
 void loop() {
@@ -55,19 +111,12 @@ int i;
 int n_pin = sizeof(inputPin)/sizeof(int); // Número de pines que serán nuestras entradas y salidas
 //Iniciamos el juego
 if (game_on == 0){
+  digitalWrite(secretCompartment,LOW);
   getInitialArray(n_levels,n_pin);
   unsigned long currentMillisBegin = millis();
   previousMillis = currentMillisBegin;
-  //Mostramos la secuencia del juego (cambiará tras el error)
-  /*for (i = 0; i < n_levels; i= i + 1){
-      leddelay = ledtime/(1+(speedfactor/n_levels)*(currentlevel - 1));
-      pinLedSerie = serie_array[i];
-      digitalWrite(pinLedSerie+7, HIGH);
-      delay(leddelay);
-      digitalWrite(pinLedSerie+7, LOW);
-      delay(100/speedfactor);
- }*/
- game_on = 1; 
+
+  game_on = 1; 
 
 }
 
@@ -78,102 +127,66 @@ i = 0;
 int buttonchange = 0;   
 mp3.stop(); 
 int j = 0; //la posición actual de la serie
-while (j < n_levels){
+currentlevel = 0;
+while (currentlevel < n_levels){
   Serial.println("CurrentLEVEL");
-  Serial.println(currentlevel);    
-    while (buttonchange == 0 && game_on==1){
-          digitalWrite(readyLed,HIGH);
-          //waitPin(n_pin,ledPin[serie_array[currentlevel-1]]);
-          digitalWrite(ledPin[serie_array[currentlevel-1]],HIGH);
-          for (i = 0; i < 4; i = i + 1){ 
-          buttonState[i] = readPIR(i+2);//digitalRead(i+2);
-          buttonchange = buttonchange + buttonState[i];
-        }
-        unsigned long currentMillis = millis();
-
-        if(currentMillis - previousMillis > interval)
-        {
-          //previousMillis = currentMillis;
-          right = 0;
-          break;
-        }
-        
-        //Serial.println(ledPin[serie_array[currentlevel-1]]);
-        delay(100);
+  Serial.println(currentlevel);
+  bool currentSensorValue =readPIR(serie_array[j]);
+  bool correct = true;
+  while(HIGH){      
+      
+      Serial.println("---------------------------------");
+      unsigned long currentMillis = millis();      
+      if(currentMillis - previousMillis > interval)
+      {
+        previousMillis = currentMillis;
+        correct = false;
+        break;
+      }   
+      digitalWrite(ledPin[serie_array[j]],HIGH);
+      currentSensorValue =readPIR(serie_array[j]);
+      if(currentSensorValue){
+        correct = true;
+        break;
+      }
+      delay(6);
     }
+    
     digitalWrite(readyLed,LOW);
-    digitalWrite(ledPin[serie_array[currentlevel-1]],LOW);
-     for (i = 0; i < 4; i = i + 1){
-        if (buttonState[i] == HIGH) {
-            digitalWrite(ledPin[i], HIGH);
-            delay(ledtime);
-            digitalWrite(ledPin[i], LOW);
-            
-            currentValue=i; 
-            buttonState[i] = LOW;
-            buttonchange = 0;
-            break;
-         }
-       } 
-        if (currentValue == serie_array[j]){
-            j++;  
-            right = 1;
-            }
-        else{
-          
-            right = 0;
-            i = 4;
-            
-        }
+    digitalWrite(ledPin[serie_array[j]],LOW);
+     
+    if (correct){
+        j++;  
+        currentlevel++;
+        Serial.println("Coorecto aumenta 1");
+        Serial.println(currentlevel);
+        unsigned long currentMillisOK = millis();
+        previousMillis =currentMillisOK;
+        
+    }else{
+        Serial.println("Error: ");
+        Serial.print(currentValue);
+        Serial.println("");
+        game_on = 0;
+        currentlevel = 0;
+        getInitialArray(n_levels,n_pin);
+        previousMillis = millis();
+        mp3.stop(); 
+        j = 0;
+        delay(30);
+    }
+
+    if(j == 1 ){
+           playSound(s[0]);
+     }
 
         // si la entrada es incorrecta se muestra una señal de error prendiendo todos los leds
-        if (right == 0){
-          delay(300);
-          i = 0;
-          game_on = 0;
-          currentlevel = 1;
-          //previousMillis = 0;
-          Serial.println("Error!!!");
-          for (i = 0; i < 4; i = i + 1){
-                 digitalWrite(ledPin[i], HIGH);
-              }
-               delay(ledtime);   
-           for (i = 0; i < 4; i = i + 1){
-                 digitalWrite(ledPin[i], LOW);   
-               }
-               delay (200);
-          for (i = 0; i < 4; i = i + 1){
-                 digitalWrite(ledPin[i], HIGH);
-              }
-               delay(ledtime);  
-           for (i = 0; i < 4; i = i + 1){
-                 digitalWrite(ledPin[i], LOW);   
-               }
-               
-               delay(500);
-               game_on = 0;
-               unsigned long currentMillisError = millis();
-               previousMillis = currentMillisError;
-               //salimos del while para iniciar un nuevo juego
-               break;
-               
-        }
+       
 
-
-    //Si se inserta el valor correcto avanzamos al siguiente elemento de la serie
-        if (right == 1){
-            currentlevel++;
-            Serial.println("Coorecto aumenta 1");
-            Serial.println(currentlevel);
-            unsigned long currentMillisOK = millis();
-            previousMillis =currentMillisOK;
-            
-            }
-         if(currentlevel == 2 ){
-           playSound(s[random(0,2)]);
-         }
+    
+       /*
         //si se llega al final de la secuencia el juego se ha ganado   
-        if (currentlevel > n_levels){
+        if (currentlevel > 4){
           Serial.println("Ha ganado el juego");
           delay(500);
           // Se crea una serie automática cuando se ha ganado el juego
@@ -192,13 +205,19 @@ while (j < n_levels){
           game_on = 0;
           currentlevel = 1;
           
-         }    
-}
+         }
+         */    
+    }
+    winner();
  
-}
+}//main loop
 
 //FUNCTIONS--------------------
-
+void winner(){
+  Serial.println("Ha ganado el juego");
+  digitalWrite(secretCompartment,LOW);
+  delay(2000);
+}
 void playSound(char sound){
   if(soundStorage==0){
     mp3.play_usb_disk(sound);
@@ -212,17 +231,15 @@ void playSound(char sound){
 //Función que inicializa el vector de la serie y la variable currentValue
 void getInitialArray(int n,int n_pin)
 {
-  Serial.println("N_PIN");
-  Serial.println(n_pin);
-  Serial.println("/N_PIN");
+
   int rand_num,random_selected;
-  for(int i=0; i<n; i=i+1){
+  for(int i=0; i<n; i++){
         serie_array[i]=0;
         currentValue = 0;
         rand_num = random(0,n_pin);
         if(i>0)
         {
-          while(rand_num == serie_array[i-1]) //Nunca habrá dos digitos iguales consecutivos
+          while(rand_num == serie_array[i-1]) //NO dos digitos  consecutivos
           {
             rand_num = random(0,n_pin);
           }
@@ -231,18 +248,39 @@ void getInitialArray(int n,int n_pin)
         random_selected = rand_num;
         serie_array[i]=random_selected;    //guardamos el valor pseudorandom seleccionado
       }
-
     
 }
 
 //Función para leer el cambio en los sensores PIR
-int readPIR(int pinPir)
-{
-  int readPin = digitalRead(pinPir);
+bool readPIR(int index)
+{ 
+  
+  float tmp = pow(sensors[index].get_units(3),2);
+  float dif = tmp - umbrals[index];
+  if(dif > 35000.0 && correctUmbral[index] == 0){
+    umbrals[index] = tmp;
+    correctUmbral[index] = 1;
+  }
+  Serial.println("");
+  Serial.print(tmp);
+  Serial.print("|");
 
-  return readPin;
+  float compare = sqrt(pow(tmp -umbrals[index],2));
+  
+  float estimate = umbrals[index]+(umbrals[index]* 0.05); 
+  Serial.print(umbrals[index]);
+  Serial.print("|");
+  Serial.print(compare );
+  Serial.println("");
+  int result = 0;
+  
+  result = compare > 20000.0 ? HIGH:LOW;
+  
+  sensors[index].power_down();              // put the ADC in sleep mode
+  //delay(5000);
+  sensors[index].power_up();
+  return result;  
 }
-
 //Función para encender el led que esperamos en la secuencia
 
 void waitPin(int n,int led)
@@ -259,3 +297,14 @@ void waitPin(int n,int led)
     }
   }
 }
+
+void getUmbrals(){
+   float cal2 = 0;
+   for (int i = 0; i < 7; i++){
+  
+        cal2=sensors[i].get_units(3);
+         umbrals[i] = pow(cal2,2);
+          delay(100);     
+   }
+}
+
